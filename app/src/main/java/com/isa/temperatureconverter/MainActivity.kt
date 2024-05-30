dbceef6bdb912ce50b2cd4c498f42d75f9a506db
Isa Dahduli <isa95dev@hotmail.com>
2024-05-30 12:39:02 +0300

    Initial commit

---
+++ app/src/main/java/com/isa/temperatureconverter/MainActivity.kt
@@ -0,0 +1,66 @@
+package com.isa.temperatureconverter
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity()
+{
+
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val etCelsius = findViewById<EditText>(R.id.et_celsius)
+        val etFahrenheit = findViewById<EditText>(R.id.et_fahrenheit)
+        val etKelvin = findViewById<EditText>(R.id.et_kelvin)
+        val btnCalculate = findViewById<Button>(R.id.btn_calculate)
+        val btnReset = findViewById<Button>(R.id.btn_reset)
+
+        btnCalculate.setOnClickListener {
+            val celsiusText = etCelsius.text.toString()
+            val fahrenheitText = etFahrenheit.text.toString()
+            val kelvinText = etKelvin.text.toString()
+
+            if (celsiusText.isNotEmpty()) {
+                val celsius = celsiusText.toDouble()
+                etFahrenheit.setText((celsius * 9 / 5 + 32).toString())
+                etKelvin.setText((celsius + 273.15).toString())
+
+                etCelsius.isEnabled = false
+                etKelvin.isEnabled = false
+                etFahrenheit.isEnabled = false
+
+            } else if (fahrenheitText.isNotEmpty()) {
+                val fahrenheit = fahrenheitText.toDouble()
+                etCelsius.setText(((fahrenheit - 32) * 5 / 9).toString())
+                etKelvin.setText((((fahrenheit - 32) * 5 / 9) + 273.15).toString())
+
+                etCelsius.isEnabled = false
+                etKelvin.isEnabled = false
+                etFahrenheit.isEnabled = false
+
+            } else if (kelvinText.isNotEmpty()) {
+                val kelvin = kelvinText.toDouble()
+                etCelsius.setText((kelvin - 273.15).toString())
+                etFahrenheit.setText(((kelvin - 273.15) * 9 / 5 + 32).toString())
+
+                etCelsius.isEnabled = false
+                etKelvin.isEnabled = false
+                etFahrenheit.isEnabled = false
+            }
+        }
+
+        btnReset.setOnClickListener {
+            etCelsius.text.clear()
+            etFahrenheit.text.clear()
+            etKelvin.text.clear()
+
+            etCelsius.isEnabled = true
+            etKelvin.isEnabled = true
+            etFahrenheit.isEnabled = true
+            }
+        }
+}
